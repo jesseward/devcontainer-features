66ab77a1d02148f6bf350089aeeeb5b5c4749f1d
Jesse Ward <jesse.ward@gmail.com>
2024-05-13 13:31:21 -0400

    Update for 10.9.1

--- test/jellyfin/version_10.9.1_debian.sh
+++ test/jellyfin/version_10.9.1_debian.sh
@@ -6,7 +6,6 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "server" /opt/jellyfin/jellyfin/jellyfin --version 2>&1| grep 10.8.13.0
-
+check "server" /opt/jellyfin/jellyfin/jellyfin --version 2>&1| grep 10.9.1
 # Report result
 reportResults
